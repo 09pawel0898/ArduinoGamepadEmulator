1592eab709716210e516fe929d8a747805414655
09pawel0898 <pawel_2014@interia.eu>
2021-04-20 13:42:10 +0200

    premake added

---
+++ premake5.lua
@@ -0,0 +1,65 @@
+workspace "ArduinoGamepadEmulator"
+	architecture "x86"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "ArduinoGamepadEmulator"
+	location "ArduinoGamepadEmulator"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		
+	}
+
+	libdirs
+	{
+		
+	}
+
+	links 
+	{ 
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
+
+		defines
+		{
+			
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+		links
+		{
+
+		}
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+
+		}
